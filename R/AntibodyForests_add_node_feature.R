e8d30954eeb9da8523f8067b21edddc16e68e60d
Daphne van Ginneken <d.van.ginneken@hotmail.com>
2024-11-05 11:04:18 +0100

    Update vignette

--- R/AntibodyForests_add_node_feature.R
+++ R/AntibodyForests_add_node_feature.R
@@ -1,35 +1,35 @@
 #' Function to add node features to an AntibodyForests-object
 #' @description Function to add node features to an AntibodyForests-object
-#' @param input AntibodyForests-object(s), output from AntibodyForests()
+#' @param AntibodyForests_object AntibodyForests-object, output from AntibodyForests()
 #' @param feature.df Dataframe with features for each node. Must contain columns sample_id, clonotype_id, barcode and the features to be added.
 #' @param feature.names Character vector with the names of the features to be added.
 #' @return Returns an AntibodyForests-object with the features added to the nodes.
 #' @export
 
-AntibodyForests_add_node_feature <- function(input,
+AntibodyForests_add_node_feature <- function(AntibodyForests_object,
                                              feature.df,
                                              feature.names){
   
   #Stop when no input is provided
-  if(missing(input)){stop("Please provide a valid input object.")}
+  if(missing(AntibodyForests_object)){stop("Please provide a valid AntibodyForests_object input object.")}
   if(missing(feature.df)){stop("Please provide a valid feature.df argument.")}
   if(missing(feature.names)){stop("Please provide a valid feature.names argument.")}
   #Check if feature.names is in feature.df
   if(!all(feature.names %in% colnames(feature.df))){stop("Not all feature.names are present in the feature.df.")}
 
   
-  out <- lapply(seq_along(input), function(sample){
-    sample_name <- names(input)[[sample]]
+  out <- lapply(seq_along(AntibodyForests_object), function(sample){
+    sample_name <- names(AntibodyForests_object)[[sample]]
     
     #Start list of clonotype names for this sample
-    clonotype_name_list <- names(input[[sample_name]])
-    out_list <- lapply(seq_along(input[[sample]]), function(clonotype){
-      clonotype_name <- names(input[[sample_name]])[[clonotype]]
+    clonotype_name_list <- names(AntibodyForests_object[[sample_name]])
+    out_list <- lapply(seq_along(AntibodyForests_object[[sample]]), function(clonotype){
+      clonotype_name <- names(AntibodyForests_object[[sample_name]])[[clonotype]]
       
       #Subset the feature.df for this specific sample and clonotype
       feature.df_sub <- feature.df[feature.df$sample_id == sample_name & feature.df$clonotype_id == clonotype_name,]
       
-      input[[sample]][[clonotype]]$nodes <- lapply(input[[sample]][[clonotype]]$nodes, function(node){
+      AntibodyForests_object[[sample]][[clonotype]]$nodes <- lapply(AntibodyForests_object[[sample]][[clonotype]]$nodes, function(node){
         for(feature in feature.names){
           #Subset the feature.df for this specific node
           feature.df_node_sub <- feature.df_sub[feature.df_sub$barcode %in% node$barcode,]
@@ -45,14 +45,14 @@ AntibodyForests_add_node_feature <- function(input,
         }
         return(node)
       })
-      return(input[[sample]][[clonotype]])
+      return(AntibodyForests_object[[sample]][[clonotype]])
     })
     #Set the names of the clonotypes
     names(out_list) <- clonotype_name_list
     return(out_list)
   })
   #Set the names of the samples
-  names(out) <- names(input)
+  names(out) <- names(AntibodyForests_object)
   
   #Change class type into AntibodyForests-object
   antibodyforests_object <- base::structure(out, class = "AntibodyForests")
